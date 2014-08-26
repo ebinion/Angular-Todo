6bea2a4ccef1c54287c6d3742a86c48fe211bedf
ebinion <ezekiel@gridshiftdesign.com>
2014-08-26 05:44:43 -0500

    Step 6 complete: http://yeoman.io/codelab/install-packages.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,29 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name todoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the todoApp
- */
-angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+// (function(){
+  /**
+   * @ngdoc function
+   * @name todoApp.controller:MainCtrl
+   * @description
+   * # MainCtrl
+   * Controller of the todoApp
+   */
+  angular.module('todoApp')
+    .controller('mainCtrl', function ($scope) {
+      $scope.todos = [
+        'Create angular code',
+        'Think about tacos',
+        'Get more of my points back from person who stole it all',
+        'Figure out why the Black Eyed Peas never went completely hip-hop'
+      ];
+
+      $scope.addTodo = function() {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+      };
+
+      $scope.removeTodo = function(index) {
+        $scope.todos.splice(index, 1);
+      };
+    });
+// })();
